70be7cf52d89d199194cb68fa841adcadd68fd95
dvir pashut <pashutdvir@gmail.com>
2022-12-22 12:53:34 +0200

    trying docker compose

--- enterypoint.sh
+++ enterypoint.sh
@@ -5,6 +5,11 @@
 ./manage.py updatecatalog
 
 ./manage.py collectstatic
-sleep 5
-echo "running server"
+
+echo "#!/bin/bash  ./manage.py collectstatic" > static.sh
+
+chmod 777 startic.sh
+./static.sh
+rm static.sh
+
 ./manage.py runserver 0.0.0.0:8000
